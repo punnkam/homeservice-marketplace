7ced6077fbe6030e2453aa832bfa050b8c0e9bbf
Punn Kamolyabutr <pkamolya@ucsd.edu>
2021-07-16 17:21:50 +0700

    routes and models

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db.js');
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.send('API Running'))
+// Connect Database
+connectDB();
 
-const PORT = process.env.PORT || 5000
+app.get('/', (req, res) => res.send('API Running'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
