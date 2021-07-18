e8100f631318a28a8f162fd983e3e402eaec144c
Punn Kamolyabutr <pkamolya@ucsd.edu>
2021-07-18 15:36:22 +0700

    add get post by id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -60,4 +60,23 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// @route   GET api/posts/:id
+// @desc    Get all posts
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
