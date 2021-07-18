0fd9533d06f31b4dc1ff2a8269b7a839aaf46500
Punn Kamolyabutr <pkamolya@ucsd.edu>
2021-07-18 15:33:21 +0700

    add get all posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -47,4 +47,17 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
