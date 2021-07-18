9045b45a99731aee7b78b13deeeb5e161a96ec3c
Punn Kamolyabutr <pkamolya@ucsd.edu>
2021-07-18 16:50:59 +0700

    add and delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -200,4 +200,37 @@ router.post(
   }
 );
 
+// @route   DELETE api/posts/comment/:id
+// @desc    Delete a comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(comment => {
+      return comment.id === req.params.comment_id;
+    });
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Check user
+    if (comment.user.toString() != req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments
+      .map(comment => comment.id.toString())
+      .indexOf(req.params.comment_id);
+    post.likes.splice(removeIndex, 1);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
