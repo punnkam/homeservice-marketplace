79a65fdbd3d1c6535d4646f8592bcb1d10f79ea9
Punn Kamolyabutr <pkamolya@ucsd.edu>
2021-07-27 13:00:00 +0700

    post display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,6 +6,7 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
+  GET_POST,
 } from './types';
 
 // Get posts
@@ -107,6 +108,26 @@ export const addPost = formData => async dispatch => {
     });
 
     dispatch(setAlert('Post Created', 'success'));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Get post
+export const getPost = postId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${postId}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    });
   } catch (error) {
     dispatch({
       type: POST_ERROR,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,6 +4,7 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
+  GET_POST
 } from '../actions/types';
 
 const initialState = {
@@ -23,6 +24,12 @@ export default function(state = initialState, action) {
         posts: payload,
         loading: false,
       };
+      case GET_POST:
+        return {
+          ...state,
+          post: payload,
+          loading: false
+        }
     case ADD_POST:
       return {
         ...state,
