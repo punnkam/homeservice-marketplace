8c21ac3faff7063bc246aa925f6e00470430b911
Punn Kamolyabutr <pkamolya@ucsd.edu>
2021-07-27 12:44:57 +0700

    add and delete post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from './types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -11,6 +17,96 @@ export const getPosts = () => async dispatch => {
       type: GET_POSTS,
       payload: res.data,
     });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Add Like
+export const addLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+//Remove Like
+export const removeLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Delete Post
+export const deletePost = postId => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: postId,
+    });
+
+    dispatch(setAlert('Post Removed', 'success'));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Add Post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(`/api/posts`, formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Post Created', 'success'));
   } catch (error) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
   auth,
+  addLike,
+  removeLike,
+  deletePost,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
   return (
@@ -21,11 +25,15 @@ const PostItem = ({
         <p class='post-date'>
           Posted on <Moment format='YYYY-MM-DD'>{date}</Moment>
         </p>
-        <button type='button' class='btn btn-light'>
+        <button onClick={e => addLike(_id)} type='button' class='btn btn-light'>
           <i class='fas fa-thumbs-up'></i>{' '}
           <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
         </button>
-        <button type='button' class='btn btn-light'>
+        <button
+          onClick={e => removeLike(_id)}
+          type='button'
+          class='btn btn-light'
+        >
           <i class='fas fa-thumbs-down'></i>
         </button>
         <Link to={`/post/${_id}`} class='btn btn-primary'>
@@ -35,7 +43,11 @@ const PostItem = ({
           )}
         </Link>
         {!auth.loading && user === auth.user._id && (
-          <button type='button' class='btn btn-danger'>
+          <button
+            onClick={e => deletePost(_id)}
+            type='button'
+            class='btn btn-danger'
+          >
             <i class='fas fa-times'></i>
           </button>
         )}
@@ -47,10 +59,15 @@ const PostItem = ({
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
